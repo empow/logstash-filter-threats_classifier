7fc8fc16585dd125e0158de825560bc5b908add3
assaf79 <assaf.abulafia@gmail.com>
2019-05-01 14:39:02 +0300

    rename

--- spec/filters/threats-classifier_spec.rb
+++ spec/filters/threats-classifier_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require_relative '../spec_helper'
-require "logstash/filters/threats-classifier"
+require "logstash/filters/threats_classifier"
 require "logstash/event"
 
 describe LogStash::Filters::ThreatSClassifier do
